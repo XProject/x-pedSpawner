4d0297e76fefa066166e7fc18b60c798a3471100
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2024-07-12 23:20:18 -0400

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version "cerulean"
+use_experimental_fxv2_oal "yes"
+lua54 "yes"
+game "gta5"
+
+name "x-pedSpawner"
+version "0.0.0"
+description "Project-X Ped Spawner"
+
+dependencies {
+    "ox_lib"
+}
+
+shared_scripts {
+    "@ox_lib/init.lua",
+    "shared/*.lua",
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+client_scripts {
+    "client/*.lua",
+}
+
+files {
+    "modules/**/*client*.lua"
+}
---
+++ modules/ped/server.lua
@@ -0,0 +1,10 @@
+---@class CPed
+---@field model  number
+---@field coords vector4
+---@field radius number
+---@field entity number
+---@field netId  number
+
+---@class CPed
+local ped = {}
+ped.__index = ped
