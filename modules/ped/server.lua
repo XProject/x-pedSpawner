68f59c8f9838474cb592ebb234758d67f766cefb
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2024-07-12 23:52:30 -0400

    feat(modules): lua woo oop class creation

--- modules/ped/server.lua
+++ modules/ped/server.lua
@@ -5,6 +5,42 @@
 ---@field entity number
 ---@field netId  number
 
----@class CPed
-local ped = {}
-ped.__index = ped
+lib.require("modules.class.shared")
+
+local Ped = class("Ped", nil, {
+    members = {
+        model = {
+            private = true,
+            value = false,
+            read_only = true
+        },
+        coords = {
+            private = true,
+            value = false,
+            read_only = true
+        },
+        radius = {
+            private = true,
+            value = false,
+            read_only = true
+        },
+        entity = {
+            private = true,
+            value = false,
+            read_only = true
+        },
+        netId = {
+            private = true,
+            value = false,
+            read_only = true
+        },
+    },
+    ctor = function(this, parent_ctor, model, coords, radius)
+        this.model = model
+        this.coords = coords
+        this.radius = radius
+    end,
+    dtor = function(this)
+        print(string.format("A %s called %s is dead", this.__class.model, this.model))
+    end
+})
